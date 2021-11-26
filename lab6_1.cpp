718b73664b90a44defeaf14ff5198d4bcb146b4a
Copter <copter.phanuwat@gamil.com>
2021-11-26 16:39:17 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
+    int x ;
+    int Even = 0;
+    int Odd = 0;
+    while(x != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+    if(x%2==0){
+        Even++;
+        if(x==0){
+            Even--;
+            }
+    }else{
+        Odd++;
+        }
+    }
+    cout << "#Even numbers = "<< Even <<"\n";
+    cout << "#Odd numbers = "<< Odd;
     return 0;
 }
